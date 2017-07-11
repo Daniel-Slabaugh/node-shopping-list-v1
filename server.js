7467dad4d626f7aaa02edb85fc341b001dc149b7
Daniel-Slabaugh <danielslabaugh@gmail.com>
2017-07-10 23:43:03 -0500

    added get function for recipes

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
-const {ShoppingList} = require('./models');
+const {ShoppingList, Recipes} = require('./models');
 
 const jsonParser = bodyParser.json();
 const app = express();
@@ -22,6 +22,15 @@ ShoppingList.create('peppers', 4);
 // all current ShoppingList items
 app.get('/shopping-list', (req, res) => {
   res.json(ShoppingList.get());
+});
+
+//add some recipies to the recipies object
+Recipes.create('coffee', 'water');
+{'coffee', 'water'}
+
+
+app.get('/recipes', (req, res) => {
+  res.json(Recipes).get()
 });
 
 app.listen(process.env.PORT || 8080, () => {
